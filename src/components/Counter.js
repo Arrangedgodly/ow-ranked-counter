1b194efb6a59116487eab45faaf7ad33dbeacd58
Arrangedgodly <arrangedgodly@gmail.com>
2023-01-25 18:31:20 -0700

    refactor: adding the increment/decrement buttons to the page

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,44 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Counter({ title, logo }) {
   const [wins, setWins] = useState(localStorage.getItem(`${title}-wins`) | 0);
-  const [losses, setLosses] = useState(localStorage.getItem(`${title}-losses`) | 0);
+  const [losses, setLosses] = useState(
+    localStorage.getItem(`${title}-losses`) | 0
+  );
+
+  const handleIncrementWins = () => {
+    if (wins === 6) {
+      setWins(0);
+      setLosses(0);
+    } else {
+      setWins(wins + 1);
+    }
+  };
+
+  const handleDecrementWins = () => {
+    if (wins === 0) {
+      return;
+    } else {
+      setWins(wins - 1);
+    }
+  };
+
+  const handleIncrementLosses = () => {
+    if (losses === 19) {
+      setWins(0);
+      setLosses(0);
+    } else {
+      setLosses(losses + 1);
+    }
+  };
+
+  const handleDecrementLosses = () => {
+    if (losses === 0) {
+      return;
+    } else {
+      setLosses(losses - 1);
+    }
+  };
 
   const handleWins = (e) => {
     if (e.target.value < 0) {
@@ -17,7 +53,7 @@ function Counter({ title, logo }) {
       setWins(e.target.value);
       localStorage.setItem(`${title}-wins`, e.target.value);
     }
-  }
+  };
 
   const handleLosses = (e) => {
     if (e.target.value < 0) {
@@ -32,55 +68,58 @@ function Counter({ title, logo }) {
       setLosses(e.target.value);
       localStorage.setItem(`${title}-losses`, e.target.value);
     }
-  }
+  };
 
   const handleReset = () => {
     setWins(0);
     setLosses(0);
     localStorage.removeItem(`${title}-wins`);
     localStorage.removeItem(`${title}-losses`);
-  }
+  };
 
   return (
     <form className="Counter">
       <h2 className="Counter__heading">{title}</h2>
-      <img
-        className='Counter__logo'
-        src={logo}
-      />
+      <img className="Counter__logo" src={logo} />
       <div className="Counter__inputs">
-        <label
-          htmlFor="wins"
-          className="Counter__label"
-        >
+        <label htmlFor="wins" className="Counter__label">
           Wins
-          <input 
-          type='number'
-          name="wins"
-          value={wins}
-          onChange={handleWins}
-          className="Counter__count"
+          <input
+            type="number"
+            name="wins"
+            value={wins}
+            onChange={handleWins}
+            className="Counter__count"
           />
+          <div className="Counter__buttons">
+            <button type="button" onClick={handleIncrementWins}>
+              +
+            </button>
+            <button type="button" onClick={handleDecrementWins}>
+              -
+            </button>
+          </div>
         </label>
-        <label
-          htmlFor="losses"
-          className="Counter__label"
-        >
+        <label htmlFor="losses" className="Counter__label">
           Losses
-          <input 
-          type='number'
-          name="losses"
-          value={losses}
-          onChange={handleLosses}
-          className="Counter__count"
+          <input
+            type="number"
+            name="losses"
+            value={losses}
+            onChange={handleLosses}
+            className="Counter__count"
           />
+          <div className="Counter__buttons">
+            <button type="button" onClick={handleIncrementLosses}>
+              +
+            </button>
+            <button type="button" onClick={handleDecrementLosses}>
+              -
+            </button>
+          </div>
         </label>
       </div>
-      <button
-        type='button'
-        className='Counter__reset'
-        onClick={handleReset}
-      >
+      <button type="button" className="Counter__reset" onClick={handleReset}>
         Reset
       </button>
     </form>
